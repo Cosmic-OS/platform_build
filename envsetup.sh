3ea6f828d818a5761bb418299188cc946e96db93
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: Preliminary support for COS product builds
    If building a cos_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the device/cm/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for COS makefiles.
    
    build: Fix fastpath code for COS_BUILD
    
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    
    build: Fix cosmic builds
    
    the setup variable wasn't exported, and was being missed by part
    of the scripts
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -130,6 +130,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^cos_") ; then
+       COS_BUILD=$(echo -n $1 | sed -e 's/^cos_//g')
+    else
+       COS_BUILD=
+    fi
+    export COS_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
