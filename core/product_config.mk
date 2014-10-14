5bbfe9e48b017f4d15e9122faa633f2007fdcaf0
Brint E. Kriebel <bekit@cyngn.com>
2014-10-14 12:43:28 -0700

    product_config: Set COS_BUILD priority above TARGET_BUILD_APPS
    Even if we are doing TARGET_BUILD_APPS, we may want to use a cm
    target for building.
    
    Change-Id: Ic8053bc679fa8b726be90e611aeaacdf1850cabc

--- core/product_config.mk
+++ core/product_config.mk
@@ -168,19 +168,20 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A COS build needs only the COS product makefiles.
+ifneq ($(COS_BUILD),)
+  all_product_configs := $(shell ls device/*/$(COS_BUILD)/cos.mk)
 else
-  ifneq ($(COS_BUILD),)
-    all_product_configs := $(shell ls device/*/$(COS_BUILD)/cos.mk)
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif
-endif
+  endif # TARGET_BUILD_APPS
+endif # COS_BUILD
 
 all_named_products :=
 
