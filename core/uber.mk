e135e0226ff5ae053913252890eb4ba2bf61167c
despairfactor <matthewdalex92@gmail.com>
2015-11-26 15:02:11 -0500

    core: add libiprouteutil to strixt exclusions

--- core/uber.mk
+++ core/uber.mk
@@ -98,7 +98,8 @@ LOCAL_DISABLE_STRICT := \
 	linker \
 	sensors.flounder \
 	libnvvisualizer \
-	libskia
+	libskia \
+	libiprouteutil
 
 LOCAL_FORCE_DISABLE_STRICT := \
 	libziparchive-host \
@@ -125,7 +126,8 @@ LOCAL_FORCE_DISABLE_STRICT := \
 	linker \
 	libc_malloc \
 	sensors.flounder \
-	libnvvisualizer
+	libnvvisualizer \
+	libiprouteutil
 
 DISABLE_STRICT := \
 	-fno-strict-aliasing
