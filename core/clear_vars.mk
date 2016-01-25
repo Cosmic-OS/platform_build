255dca7b72f88eb33ac0d5a4a059cdfe75e781f7
Ricardo Cerqueira <ricardo@cyngn.com>
2016-01-25 22:19:00 +0530

    binary: Allow specifying a path for generated proto headers
    protobuf compilation defaults to dropping the generated objects and
    headers onto a path derived from the origin source path. This poses
    a problem with some of our repositories since they're not placed in
    their original paths, and may include hard references to pb headers
    assuming the original directory.
    
    Typical use of this will be to place a
    
    LOCAL_PROTOC_OUTPUT := <original repo path>
    
    line in the relevant module block (for example, "hardware/ril" for our
    "hardware/ril-caf")

--- core/clear_vars.mk
+++ core/clear_vars.mk
@@ -368,6 +368,7 @@ LOCAL_INIT_RC_64:=
 LOCAL_JAVA_LANGUAGE_VERSION:=
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY:=
 LOCAL_RRO_THEME:=
+LOCAL_PROTOC_OUTPUT:=
 
 # Trim MAKEFILE_LIST so that $(call my-dir) doesn't need to
 # iterate over thousands of entries every time.
