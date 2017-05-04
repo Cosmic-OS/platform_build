2084856a31cc8cf43a644cec8bb93fe0eb34911a
ezio84 <brabus84@gmail.com>
2017-05-04 10:41:16 +0200

    Stop overwriting audio_effects.conf
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -64,7 +64,6 @@ PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
 	frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
-	frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf \
 	hardware/libhardware_legacy/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 include $(SRC_TARGET_DIR)/product/emulator.mk
