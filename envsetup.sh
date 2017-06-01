4e8fffa9f51866b1226acd28a4078fd4b2726af1
Sagar Shah <sagarshah2326@gmail.com>
2017-06-01 17:08:28 +0530

    Fixes

--- envsetup.sh
+++ envsetup.sh
@@ -654,7 +654,7 @@ function lunch()
     check_product $product
     if [ $? -ne 0 ]
     then
-        # if we can't find a product, try to grab it off the SLIM github
+        # if we can't find a product, try to grab it off the COS github
         T=$(gettop)
         pushd $T > /dev/null
         build/tools/roomservice.py $product
