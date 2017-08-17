85b2a9b7912744691eb35495cf37482a1a266a0b
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:20:29 +0200

    core: Include QCOM/MTK targets
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -971,6 +971,12 @@ $(eval include device/cosmic/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/cos/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/cos/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
