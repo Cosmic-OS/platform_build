5ff66090185634ad457a84e527ee50f541bb1131
Chris <chris@cypheros.co>
2018-01-01 20:27:28 -0500

    target: Build black theme
    Change-Id: Ib38256608094065a909fbaea8e6e22725a193ca1
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- target/product/core.mk
+++ target/product/core.mk
@@ -53,6 +53,7 @@ PRODUCT_PACKAGES += \
     PrintRecommendationService \
     ProxyHandler \
     Settings \
+    SettingsBlackThemeOverlay \
     SettingsDarkThemeOverlay \
     SharedStorageBackup \
     StorageManager \
