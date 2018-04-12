CypherOS/build

build: Include custom BoardConfig from vendor
Change-Id: I2fa56d400ca2adf371dc3edb37e47619c7dd0a8e

core/config.mk
@@ -184,6 +184,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
+-include vendor/aoscp/configs/BoardConfigAOSCP.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:

Cosmic-OS/platform_build

core/config.mk
@@ -184,6 +184,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
+-include vendor/cos/config/BoardConfigCOSMIC.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers: