RenanQueiroz/build-1

config: use AOSP RIL for aosp builds
Change-Id: I1ffc45426c16920d138bfd6b3940f16899b70e65

core/config.mk
@@ -227,6 +227,10 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 # General entries for project pathmap.  Any entries listed here should
 # be device and hardware independent.
 $(call project-set-path-variant,recovery,RECOVERY_VARIANT,bootable/recovery)
+ifeq ($(CUSTOM_BUILD),)
+# AOSP targets should use AOSP RIL
+$(call project-set-path,ril,hardware/ril)
+endif
 
 -include vendor/extra/BoardConfigExtra.mk
 -include vendor/aosp/config/BoardConfig.mk

Cosmic-OS/platform_build

Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

core/config.mk
@@ -227,6 +227,10 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 # General entries for project pathmap.  Any entries listed here should
 # be device and hardware independent.
 $(call project-set-path-variant,recovery,RECOVERY_VARIANT,bootable/recovery)
+ifeq ($(COS_BUILD),)
+# AOSP targets should use AOSP RIL
+$(call project-set-path,ril,hardware/ril)
+endif
 
 -include vendor/extra/BoardConfigExtra.mk
 -include vendor/cos/config/BoardConfig.mk