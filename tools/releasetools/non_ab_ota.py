bdad1478b483062e4c4ed8508a6e42288f1b4236
Amitava Mitra <mitraamitava7@gmail.com>
2018-09-02 08:55:36 +0530

    releasetools: Add our ASCII logo for recovery
    Squashed of:
    1. include our dope ASCII logo for twrp
    
    Change-Id: I4d69ce91e77ece2730d1e3d72308611ad07c8a80
    Signed-off-by: Amitava Mitra <mitraamitava7@gmail.com>
    
    2.
    tools: releasetools: improvise our ASCII logo
    
    Change-Id: I32e83e0010210f1c957c96158731512ab060e21c
    Signed-off-by: Amitava Mitra <mitraamitava7@gmail.com>
    
    [kubersharma001]:
    * Adapt to android-12.0
    Ref: https://android.googlesource.com/platform/build/+/cff4d7606dd195d8c2726867024b310a23a4ac8b
    
    Change-Id: I4d69ce91e77ece2730d1e3d72308611ad07c8a80
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -221,6 +221,34 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  android_version = target_info.GetBuildProp("ro.build.version.release")
+  build_id = target_info.GetBuildProp("ro.build.id")
+  build_date = target_info.GetBuildProp("ro.build.date")
+  security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.cos.device")
+
+  script.Print("----------------------------------------------");
+  script.Print("		                              ");
+  script.Print("	  $$$$$$\   $$$$$$\   $$$$$$\         ");
+  script.Print("	 $$  __$$\ $$  __$$\ $$  __$$\        ");
+  script.Print("	 $$ /  \__|$$ /  $$ |$$ /  \__|       ");
+  script.Print("	 $$ |      $$ |  $$ |\$$$$$$\         ");
+  script.Print("	 $$ |      $$ |  $$ | \____$$\        ");
+  script.Print("	 $$ |  $$\ $$ |  $$ |$$\   $$ |       ");
+  script.Print("	 \$$$$$$  | $$$$$$  |\$$$$$$  |       ");
+  script.Print("	  \______/  \______/  \______/        ");
+  script.Print("	                                      ");
+  script.Print("	                                      ");
+  script.Print("	   Cosmic-OS v8.0 - Transit           ");
+  script.Print("	                                      ");
+  script.Print("----------------------------------------------");
+  script.Print(" Android version: %s"%(android_version));
+  script.Print(" Build id: %s"%(build_id));
+  script.Print(" Build date: %s"%(build_date));
+  script.Print(" Security patch: %s"%(security_patch));
+  script.Print(" Device: %s"%(device));
+  script.Print("----------------------------------------------");
+
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
