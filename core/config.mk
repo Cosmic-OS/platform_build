4e771aaae7096be097e683b6d4f9299613e8aaf1
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-05 21:35:38 -0700

    core: Include Custom Sepolicy if repo exists
    Change-Id: If799106d2da6d8c303430e316edff365a22f7c99

--- core/config.mk
+++ core/config.mk
@@ -1187,9 +1187,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(COSMIC_BUILD),)
+ifneq ($(wildcard device/cosmic/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/cos/sepolicy/common/sepolicy.mk)
+$(eval include device/cosmic/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
