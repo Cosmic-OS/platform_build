3322514e8da97a1752bc4e9bae03bce86082a2d2
Sagar Shah <sagarshah2326@gmail.com>
2019-11-30 18:18:42 +0100

    build/make: Fix Sepolicy path
    Change-Id: I32db1b69ea074ab90184d55ca54f2c4856329b06
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1188,10 +1188,10 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(COSMIC_BUILD),)
-ifneq ($(wildcard device/cosmic/sepolicy/common/sepolicy.mk),)
+ifneq ($(wildcard device/custom/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/cosmic/sepolicy/common/sepolicy.mk)
+$(eval include device/custom/sepolicy/common/sepolicy.mk)
 endif
 endif
 
