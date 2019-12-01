Wave-Project/build

core: Include QCOM HAL pathmap configuration from our vendor

core/config.mk
@@ -224,6 +224,10 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(WAVE_BUILD),)
+include vendor/wave/build/core/qcom_target.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1090,7 +1094,7 @@ ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
 $(warning BOARD_USES_QCOM_HARDWARE is not set!)
 $(error WaveOS is a CAF based ROM - it's for qualcomm devices only!)
 endif
-$(eval include device/wave/sepolicy/common/sepolicy.mk)
+include device/wave/sepolicy/common/sepolicy.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk

core/pathmap.mk
@@ -93,3 +93,5 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+-include vendor/wave/build/core/pathmap.mk

Cosmic-OS/platform_build

Change-Id: I3aafbd1aef22f9e8550c3216f1fb6f3f34176092
Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

core/config.mk
@@ -235,6 +235,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 ifneq ($(COSMIC_BUILD),)
 include vendor/cos/config/BoardConfigCosmic.mk
+include vendor/cos/build/core/qcom_target.mk
 endif
 
 # The build system exposes several variables for where to find the kernel

core/pathmap.mk
@@ -93,3 +93,5 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+-include vendor/cos/build/core/pathmap.mk